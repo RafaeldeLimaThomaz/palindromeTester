ceee2ceeea07309fd485927243a2971b125afb1b
RafaeldeLimaThomaz <79thomaz@gmail.com>
2020-05-27 14:10:27 -0300

    initialize repository

---
+++ index.js
@@ -0,0 +1,137 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+// checks emptyness or blankness
+String.prototype.blank = function() {
+  if (Array.from(this) == '/\^s+^/')
+    return true;
+}
+
+String.prototype.last = function() {
+  return Array.from(this).slice(-1);
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherw.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === 
+    this.processedContent().reverse();
+  }
+
+  // Makes the phrase LOUDER.
+  this.louder = function(){
+    return this.content.toUpperCase();
+  };
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent(){
+    return this.processor(this.translation);
+  }
+
+  
+}
+
+TranslatedPhrase.prototype = new Phrase();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function emailParts(email){
+  let lowCaseEmail = email.toLowerCase();
+  let userName = "";
+  let domain = "";
+  let i = 0;
+  while(lowCaseEmail.charAt(i) != '@'){
+    userName = userName + lowCaseEmail.charAt(i);
+    i++;
+  }
+  i ++;
+  while(lowCaseEmail.charAt(i) != '.'){
+
+    domain = domain + lowCaseEmail.charAt(i);
+    i++;
+  }
+
+  return "userName: " + userName + "   " + "domain: " + domain;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
